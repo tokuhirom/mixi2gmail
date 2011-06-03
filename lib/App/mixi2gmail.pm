5240cb640426cc8079e6ff40f876b811a9d7948f
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-06-04 06:45:13 +0900

    initial import

---
+++ lib/App/mixi2gmail.pm
@@ -0,0 +1,39 @@
+package App::mixi2gmail;
+use strict;
+use warnings;
+use 5.008008;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+App::mixi2gmail -
+
+=head1 SYNOPSIS
+
+  use App::mixi2gmail;
+
+=head1 DESCRIPTION
+
+App::mixi2gmail is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
